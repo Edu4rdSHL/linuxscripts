588b1f5d49090e4c42b478d1186f4e44bee7f77e
Edu4rdSHL <edu4rdshl@protonmail.com>
2021-05-13 16:53:44 -0500

    Update
    Signed-off-by: Edu4rdSHL <edu4rdshl@protonmail.com>

--- etc/systemd/nspawn/20-machines-profile.sh
+++ etc/systemd/nspawn/20-machines-profile.sh
@@ -1,5 +1,5 @@
 export DISPLAY=":0.0"
 export LC_ALL="en_US.UTF-8"
-export PULSE_SERVER="/tmp/pulse_server"
+export PULSE_SERVER="unix:/tmp/pa_containers.socket"
 # Fix Qt4 GUIs ("invalid Pixmap or Window parameter")
 export QT_X11_NO_MITSHM=1
